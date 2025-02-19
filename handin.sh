950208bb6c29f1d7d9c86996994bfe8cfb70552d
Olle Logdahl <dv20oll@cs.umu.se>
2025-02-19 16:20:59 +0100

    ahanding

--- handin.sh
+++ handin.sh
@@ -6,5 +6,5 @@ rm -f handin.zip handin-assets.zip
 
 typst c --root report report/report.typ
 zip -r handin.zip makefile README.md src/ vendor/ shaders/ report/report.pdf
-zip handin.zip -j ~/bin/glslc
 zip -r handin-assets.zip scenes/ assets/
+zip handin-assets.zip -j ~/bin/glslc
